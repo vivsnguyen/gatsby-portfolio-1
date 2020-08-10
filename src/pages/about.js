68a7db29a3dcc54376797109feb26509c49968b1
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-08-10 16:31:01 -0700

    used gravatar for about page image

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const AboutPage = () => (
         <h1>About</h1>
           <div className="info">
             <ul>
-            <p><img src="../images/self_pic.png" width="100" height="100"></img></p>
+            <p><img src="https://en.gravatar.com/userimage/169635224/5e849dc85e115edbdb936bcdba2ff606.png?size=250"></img></p>
             <p>Hello! My name is Vivi. I graduated in 2019 with a Bachelor's in Cognitive Science from UC Berkeley. Throughout my college career, I've dabbled in teaching a lot of different subjects (reading, math, science, music, and computer science!).</p>
 
             <p>I've taken courses in computer science in undergrad and worked on Python based research projects in psychology and in education. I also taught computer science for The Coder School, which has a very open-ended learning curriculum. Teaching computer science is where I learned to debug and have fun with coding.</p>
